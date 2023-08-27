37ed09a8be00ab34233c8f47d4f1c94439225200
Guilherme Pedroza <109224884+Gui-Pedroza@users.noreply.github.com>
2023-08-27 14:14:45 -0300

    Projeto finalizado

--- script/script.js
+++ script/script.js
@@ -1,20 +1,25 @@
 function changeFrameImg(id) {
-    const iframe = document.getElementById('iframe')
-    const nomeIcone = id.toString()
-    switch (nomeIcone) {        
-        case youtube_ico:
+    const iframe = document.getElementById('iframe')    
+    switch (id) {        
+        case 'youtube_ico':
             iframe.src = 'pages/youtube.html'
+            break;
         case 'github_ico':
             iframe.src = 'pages/github.html'
+            break;
         case 'instagram_ico':
             iframe.src = 'pages/instagram.html'
+            break;
         case 'twitter_ico':
             iframe.src = 'pages/twitter.html'
+            break;
         case 'facebook_ico':
             iframe.src = 'pages/facebook.html'
+            break;
         default:
             iframe.src = 'pages/home.html'
     }
-    console.log(nomeIcone)
+    console.log(id)
+    console.log(typeof id)
     console.log(iframe.src)
 }
