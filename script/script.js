a2bacb2c99088e0d3cba2a53a0a3feb9f3728aeb
Guilherme Pedroza <109224884+Gui-Pedroza@users.noreply.github.com>
2023-08-26 21:04:37 -0300

    Criação das páginas e links paralelos

--- script/script.js
+++ script/script.js
@@ -1,7 +1,20 @@
 function changeFrameImg(id) {
-    
-    let icone = document.querySelector(`#${id} img`)
-    // fazer as transicoes via CSS mesmo
-    
-    console.log(id)
+    const iframe = document.getElementById('iframe')
+    const nomeIcone = id.toString()
+    switch (nomeIcone) {        
+        case youtube_ico:
+            iframe.src = 'pages/youtube.html'
+        case 'github_ico':
+            iframe.src = 'pages/github.html'
+        case 'instagram_ico':
+            iframe.src = 'pages/instagram.html'
+        case 'twitter_ico':
+            iframe.src = 'pages/twitter.html'
+        case 'facebook_ico':
+            iframe.src = 'pages/facebook.html'
+        default:
+            iframe.src = 'pages/home.html'
+    }
+    console.log(nomeIcone)
+    console.log(iframe.src)
 }
